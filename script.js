484ea227d31459b12f80b87e0b71421b5eee26e2
Hamsensen-sup <ayagjosemari@gmail.com>
2024-11-04 15:02:19 +0800

    some changes

--- script.js
+++ script.js
@@ -20,20 +20,20 @@ const fetchApi = async () => {
         const json = await response.json()
 
         json.forEach((element, index) => {
-            const allDefinitions = element.meanings
+            const wordMeaning = element.meanings
             const h1 = document.createElement("h1")
             h1.classList.add("word")
             h1.textContent = `${index + 1}.${element.word.toUpperCase()}`
             word.appendChild(h1)
 
-            allDefinitions.forEach((element) => {
-                const newDefinition = element.definitions
+            wordMeaning.forEach((element) => {
+                const wordDefinitions = element.definitions
                 const h2 = document.createElement("h2")
                 h2.classList.add("part-of-speech")
                 h2.textContent = `PART OF SPEECH: ${element.partOfSpeech.toUpperCase()}`
                 word.appendChild(h2)
 
-                newDefinition.forEach((element, index) => {
+                wordDefinitions.forEach((element, index) => {
                     const paragraph = document.createElement("p")
                     paragraph.classList.add("words")
                     paragraph.textContent = `${index + 1}. ${element.definition}`
